e05de3be18711e39933881c10e868fd3a8bb0b31
FelipePassarela <felipepassarela8@hotmail.com>
2023-08-28 00:59:28 -0300

    script de diff atualizado

--- 02_recursao/diff.sh
+++ 02_recursao/diff.sh
@@ -3,14 +3,6 @@ gcc ./main.c -o ./main.exe -lm
 
 for i in ./input/*; do 
     ./main.exe < $i > ./outmain/$(basename $i);
-
-    # Adiciona uma linha em branco no final dos arquivos de saída
-    if [ "$(tail -c 1 ./output/$(basename $i))" != "" ]; then
-        echo "" >> ./output/$(basename $i);
-    fi
-    if [ "$(tail -c 1 ./outmain/$(basename $i))" != "" ]; then
-        echo "" >> ./outmain/$(basename $i);
-    fi
 done
 
 diff -w ./output ./outmain
