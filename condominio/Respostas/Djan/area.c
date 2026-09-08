d3efd7acc67ce8aadb960041b52fe19c65e24381
Djan <djan.ferreira@edu.ufes.br>
2026-09-08 18:42:10 -0300

    Trabalhando

--- condominio/Respostas/Djan/area.c
+++ condominio/Respostas/Djan/area.c
@@ -1,7 +1,5 @@
-
-
-#ifndef _AREA_H
-#define _AREA_H
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_TAM_NOME 100
 #define MAX_TAM_ID 5
@@ -17,31 +15,53 @@ typedef struct
  * Função que lê uma área comum de acordo com o formato de entrada descrito na
  * especificação e retorna a Area lida.
  */
-Area lerArea();
+Area lerArea(){
+    Area a;
+    
+    scanf("%s",a.nome);
+    scanf("%s",a.id);
+    scanf("%d",&a.capacidade);
+
+    return a;
+}
 
 /**
  * Função que imprime todas as informações de uma Area conforme o formato
  * descrito na especificação. Dica: veja o arquivo de saída nos casos de teste.
  *
  */
-void imprimeArea(Area a);
+void imprimeArea(Area a){
+    printf("Area reservada:\nNome: %s\nId: %s\nCapacidade: %d convidados(s)\nData da reserva: ",a.nome,a.id,a.capacidade);
+}
 
 /*
  * Função que compara se duas áreas são iguais através do
  * id delas.
  * Retorna 1 se forem iguais e 0 caso contrário
  */
-int comparaArea(Area a1, Area a2);
+int comparaArea(Area a1, Area a2){
+    if(strcmp(a1.id,a2.id) == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 /*
  * Função que verifica se o id passado é igual ao id da área passada
  * Retorna 1 se for igual e 0 caso contrário
  */
-int verificaIdArea(Area a, char *id);
+int verificaIdArea(Area a, char *id){
+    if(strcmp(a.id, id)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 /**
  * Função que retorna a capacidade de uma área comum.
  */
-int getCapacidadeArea(Area a);
-
-#endif
+int getCapacidadeArea(Area a){
+    return a.capacidade;
+}
--- condominio/Respostas/Djan/morador.c
+++ condominio/Respostas/Djan/morador.c
@@ -1,8 +1,5 @@
-
-
-#ifndef _MORADOR_H
-#define _MORADOR_H
-
+#include <stdio.h>
+#include <string.h>
 #include "data.h"
 
 #define MAX_TAM_NOME 100
@@ -11,7 +8,6 @@
 
 typedef struct
 {
-
     char nome[MAX_TAM_NOME];
     char cpf[MAX_TAM_CPF];
     Data dataNasc;
@@ -24,31 +20,58 @@ typedef struct
  *  especificação
  *  Retorna o Morador lido
  */
-Morador lerMorador();
+Morador lerMorador(){
+    Morador m;
+
+    scanf("%s",m.nome);
+    scanf("%s",m.cpf);
+    lerData();
+    scanf("%s",m.idResidencia);
+
+    return m;
+}
 
 /**
  * Função que imprime todas as informações de uma Morador conforme o formato
  * descrito na especificação. Dica: veja o arquivo de saída nos casos de teste.
  *
  */
-void imprimeMorador(Morador m);
+void imprimeMorador(Morador m){
+    printf("Morador:\n");
+    printf("Nome: %s\n",m.nome);
+    printf("CPF: %s\n",m.cpf);
+    imprimeData(m.dataNasc);
+    printf("Residência: %s\n",m.idResidencia);
+}
 
 /**
  * Verifica se dois moradores são iguais. A comparação é feita usando apenas o CPF.
  * Se for exatamente o mesmo, retorna 1. Caso contrário, retorna 0.
  */
-int comparaMorador(Morador m1, Morador m2);
+int comparaMorador(Morador m1, Morador m2){
+    if(strcmp(m1.cpf,m2.cpf) == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 /**
  * Verifica se um dado CPF pertence ao morador.
  * Se for exatamente o mesmo, retorna 1. Caso contrário, retorna 0.
  */
-int verificaCPFMorador(Morador m1, char *cpf);
+int verificaCPFMorador(Morador m1, char *cpf){
+    if(strcmp(m1.cpf, cpf) == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
 /*
  *  Função que calcula a idade de um morador em relação a uma data de referência
  *  Retorna a idade do morador em anos
  */
-int calculaIdadeMorador(Morador m, Data dataRef);
-
-#endif
+int calculaIdadeMorador(Morador m, Data dataRef){
+    return calcularDiffAnosData(m.dataNasc,dataRef);
+}
