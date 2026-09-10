7440204122a7a919f91debcce893488b84342f2b
Djan <djan.ferreira@edu.ufes.br>
2026-09-10 10:53:25 -0300

    Trabalhar e entender os conceitos

---
+++ 04_TAD_simples/TAD_06/Respostas/Djan/matrix_utils.h
@@ -0,0 +1,96 @@
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+
+#define MAX_MATRIX_SIZE 10
+
+typedef struct Matrix{
+    int rows;
+    int cols;
+    int data[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
+} tMatrix;
+
+/**
+ * @brief Cria uma matriz com o número de linhas e colunas especificado.
+ * @param rows O número de linhas na matriz.
+ * @param cols O número de colunas na matriz.
+ * @return A matriz criada.
+ */
+tMatrix MatrixCreate(int rows, int cols);
+
+/**
+ * @brief Lê uma matriz da entrada padrão.
+ * @param matrix A matriz a ser lida.
+ * @return A matriz lida.
+ */
+tMatrix MatrixRead(tMatrix matrix);
+
+/**
+ * @brief Imprime uma matriz na saída padrão.
+ * @param matrix A matriz a ser impressa.
+ */
+void MatrixPrint(tMatrix matrix);
+
+/**
+ * @brief Verifica se é possível somar duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return 1 se for possível somar as matrizes, 0 caso contrário.
+ */
+int PossibleMatrixSum(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Verifica se é possível subtrair duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return 1 se for possível subtrair as matrizes, 0 caso contrário.
+ */
+int PossibleMatrixSub(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Verifica se é possível multiplicar duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return 1 se for possível multiplicar as matrizes, 0 caso contrário.
+ */
+int PossibleMatrixMultiply(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Soma duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return O resultado da soma.
+ */
+tMatrix MatrixAdd(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Subtrai duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return O resultado da subtração.
+ */
+tMatrix MatrixSub(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Multiplica duas matrizes.
+ * @param matrix1 A primeira matriz.
+ * @param matrix2 A segunda matriz.
+ * @return O resultado da multiplicação.
+ */
+tMatrix MatrixMultiply(tMatrix matrix1, tMatrix matrix2);
+
+/**
+ * @brief Transpõe uma matriz.
+ * @param matrix A matriz a ser transposta.
+ * @return A matriz transposta.
+ */
+tMatrix TransposeMatrix(tMatrix matrix);
+
+/**
+ * @brief Multiplica uma matriz por um escalar.
+ * @param matrix A matriz a ser multiplicada.
+ * @param scalar O escalar pelo qual a matriz será multiplicada.
+ * @return O resultado da multiplicação.
+ */
+tMatrix MatrixMultiplyByScalar(tMatrix matrix, int scalar);
+
+#endif
