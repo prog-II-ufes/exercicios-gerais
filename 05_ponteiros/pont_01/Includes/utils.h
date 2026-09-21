934cfdc0636049097965722604c61610e3941572
jhonatanleao <jhony_leao@hotmail.com>
2026-09-21 15:30:00 -0300

    atualizando o PDF do exercicios de ponteiros e alocacao dinamica/01_geral e a correcao no utils.h do pont_01

--- 05_ponteiros/pont_01/Includes/utils.h
+++ 05_ponteiros/pont_01/Includes/utils.h
@@ -8,8 +8,8 @@
  * 
  * @param n1 Ponteiro para a primeira variável a ser trocada.
  * @param n2 Ponteiro para a segunda variável a ser trocada.
- * @return 1 se a troca foi realizada com sucesso, 0 caso contrário.
+ * @return 1 se os dois ponteiros forem trocados de lugar, 0 caso contrário (ou seja, a troca não foi realizada). 
  */
-int TrocaInterios(int * n1, int * n2);
+int TrocaInteiros(int * n1, int * n2);
 
 #endif
